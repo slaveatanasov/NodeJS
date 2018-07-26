6d8d51463fb49460e2b736470d6469e312ec0ba1
slaveatanasov <36505476+slaveatanasov@users.noreply.github.com>
2018-07-26 21:33:09 +0200

    Chas 1

---
+++ index.js
@@ -0,0 +1,56 @@
+// console.log("Hello world.");
+
+// var integer1 = 5;
+// var integer2 = "10";
+// var sum = integer1+integer2;
+
+// console.log(integer1 + "+" + integer2 + "=" + sum);
+
+// var name = "Slave";
+// var pass = "0000";
+// var nameUpdate = name.toLowerCase();
+
+// if (nameUpdate == "slave"){
+// 	console.log("Your name is "+name);
+// }else{
+// 	console.log("Not.");
+// }
+
+// if (name == "Slave" || name = "slave"){
+// 	console.log("Hello " + name + ".")}
+// 	else {
+// 		console.log("Unsucessful.")
+// 	}
+
+// switch (name) {
+// 	case "Slave": console.log("Your name is "+name);
+// 	break;
+// 	case "slave": console.log("Your name is "+name);
+// 	break;
+// 	default: console.log("Unsucessful");
+// }
+
+// var firstName = "Slave";
+// var lastName = "Atanasov";
+
+// var fullName = firstName.concat(lastName);
+// console.log(fullName);
+
+// var boolean = true;
+// console.log(boolean);
+
+var integer = [3, 4, 5, 6, 7, 8, 9, 12];
+
+// integer[100] = 50;
+// console.log(integer);
+
+// for (let i = 0; i < integer.length; i++){
+//  console.log(integer[i] + ": ");
+//  if(integer[i] % 3 == 0){
+// 	console.log("Buzz");
+// 	}
+// 	if (integer[i] % 4 == 0){
+// 		console.log("Fizz");
+// 	}
+// 	console.log("\n");
+// }
